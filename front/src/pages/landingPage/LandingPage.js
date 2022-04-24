d0a40c0178e8626e4490cac2666cbadb81cd5bc2
Manuel Sosa <mr.sosa@uniandes.edu.co>
2022-04-23 20:26:05 -0500

    Landing page v4

--- front/src/pages/landingPage/LandingPage.js
+++ front/src/pages/landingPage/LandingPage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './LandingPage.scss';
+
 export const LandingPage = () => {
     return (
       <>
-        <div className='landing'>
-          
-          <h1 className='title'>Wheels</h1>
-          <div className='buttons'>
-            <button className='button-style'><a className='button-text'>Log In</a></button>
-            <button className='button-style'><a className='button-text'>Sign Up</a></button>
+        <div className='landing'>          
+          <h1 className='landing-title'>Wheels</h1>
+          <div className='landing-buttons'>
+            <button className='landing-buttons-style'><a className='landing-buttons-style-text'>Log In</a></button>
+            <button className='landing-buttons-style'><a className='landing-buttons-style-text'>Sign Up</a></button>
           </div>
         </div>
       </>
