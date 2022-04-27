ffd2256685a1aa3dcabe52d90672492d75a29172
Manuel Sosa <mr.sosa@uniandes.edu.co>
2022-04-26 21:34:35 -0500

    No Page v3

--- front/src/pages/noPage/NoPage.js
+++ front/src/pages/noPage/NoPage.js
@@ -1,11 +1,20 @@
 import React from "react";
 import './NoPage.scss';
+import noFound from './../../data/404error.png';
+import { Link } from "react-router-dom";
 
 export const NoPage = () => {
     return(
         <>
             <div className="NoPage">
-                <h1>Error 404 - Page No found</h1>
+                <div className="NoPage-1">
+                    <img className="NoPage-1-img" src={noFound} />
+                </div>
+                <div className="NoPage-2">
+                    <h1>Oops, Page No Found</h1>
+                    <h3> The page you were looking for does not exist</h3>
+                    <button className="NoPage-2-button"><Link className="NoPage-2-button-text" to='/home'>Go Home</Link></button>
+                </div>
             </div>
         </>
     );
