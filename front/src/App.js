cbc16dab1edf5e2aa200e8dc7ad42a9e3e7173f2
Manuel Sosa <mr.sosa@uniandes.edu.co>
2022-04-30 11:31:13 -0500

    No Page v5

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, Outlet } from 'react-router-dom';
-import logo from './logo.svg';
+import { UserContext } from './context/UserContext';
 import './App.css';
+
 import { LandingPage } from './pages/landingPage/LandingPage';
 import { Home } from './pages/home/Home';
 import { NoPage } from './pages/noPage/NoPage';
+import { LogIn } from './pages/logIn/LogIn';
 
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<LandingPage/>}/>
-          <Route path='/home' element={<Home/>}/>
-          <Route path='*' element={<NoPage/>}/>
-        </Routes>
-      </Router>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Router>
+          <Routes>
+            <Route path='/' element={<LandingPage/>}/>
+            <Route path='/home' element={<Home/>}/>
+            <Route path='*' element={<NoPage/>}/>
+            <Route path='/login' element={<LogIn/>}/>
+          </Routes>
+        </Router>
+      </UserContext.Provider>
     </>
   );
 }
--- front/src/pages/noPage/NoPage.js
+++ front/src/pages/noPage/NoPage.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useContext } from 'react';
+import { Link } from "react-router-dom";
+import { UserContext } from './../../context/UserContext';
 import './NoPage.scss';
 import noFound from './../../data/404error.png';
-import { Link } from "react-router-dom";
 
 export const NoPage = () => {
+    const { user } = useContext(UserContext);
     return(
         <>
             <div className="NoPage">
@@ -13,7 +16,9 @@ export const NoPage = () => {
                 <div className="NoPage-2">
                     <h1>Oops, Page No Found</h1>
                     <h3> The page you were looking for does not exist</h3>
-                    <Link to='/home'><button className="NoPage-2-button">Go Home</button></Link>
+                    {user ? 
+                    <Link to='/home'><button className="NoPage-2-button">Go Home</button></Link> : 
+                    <Link to='/'><button className="NoPage-2-button">Go Home</button></Link>}
                 </div>
             </div>
         </>
