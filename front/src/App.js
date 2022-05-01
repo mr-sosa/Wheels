c127c7990658cff7906e2bed1a69ede413a8f704
Manuel Sosa <mr.sosa@uniandes.edu.co>
2022-05-01 14:26:28 -0500

    Login route

--- front/src/App.js
+++ front/src/App.js
@@ -7,7 +7,7 @@ import { LandingPage } from './pages/landingPage/LandingPage';
 import { Home } from './pages/home/Home';
 import { CrearViaje } from './pages/crearViaje/CrearViaje';
 import { NoPage } from './pages/noPage/NoPage';
-//import { LogIn } from './pages/logIn/LogIn';
+import { LogIn } from './pages/logIn/LogIn';
 
 
 function App() {
@@ -21,7 +21,7 @@ function App() {
             <Route path='/home' element={<Home/>}/>
             <Route path='*' element={<NoPage/>}/>
             <Route path='/crearViaje' element={<CrearViaje/>}/>
-            {/* <Route path='/login' element={<LogIn/>}/> */}
+            <Route path='/login' element={<LogIn/>}/>
           </Routes>
         </Router>
       </UserContext.Provider>
