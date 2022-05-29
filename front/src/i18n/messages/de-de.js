e96ff1119ebada583deac33759420867e3605b4b
Manuel Sosa <mr.sosa@uniandes.edu.co>
2022-05-28 19:38:53 -0500

    I18N v1

---
+++ front/src/i18n/messages/de-de.js
@@ -0,0 +1,16 @@
+import { LOCALES } from '../locales';
+
+const german = {
+  [LOCALES.GERMAN]: {
+    hello: 'Hallo',
+    gallery: 'Galerie',
+    username: 'Nutzername',
+    password: 'Passwort',
+    login: 'Anmeldung',
+    logout: 'Ausloggen',
+    todo: 'Todo',
+    profile: 'Profile',
+  },
+};
+
+export default german;
---
+++ front/src/i18n/messages/en-us.js
@@ -0,0 +1,18 @@
+import { LOCALES } from '../locales';
+
+const english = {
+  [LOCALES.ENGLISH]: {
+    hello: 'Hello',
+    gallery: 'Gallery',
+    username: 'Username',
+    password: 'Password',
+    login: 'Login',
+    logout: 'Logout',
+    todo: 'Todo',
+    profile: 'Profile',
+    products: 'Products',
+    contact: 'Contacts',
+  },
+};
+
+export default english;
