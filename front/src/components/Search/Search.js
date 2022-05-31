1dadf0973497d5a375d5bdfc6b1232b04d3e4ef5
David Ruiz <daleruvi@gmail.com>
2022-05-30 22:28:16 -0500

    run build funcionando

--- front/src/components/Search/Search.js
+++ front/src/components/Search/Search.js
@@ -3,59 +3,12 @@ import useCollapse from 'react-collapsed';
 import { TripContext } from '../../context/TripContext';
 import { Post } from '../Post/Post';
 import './Search.scss';
+import { useForm } from "../../hooks/useForm";
 
 
-function LocationsCollapsible() {
-    const [isExpanded, setExpanded] = useState(false)
-    const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded });
-    
-    return (
-        <div className="Locations">
-            <div className="Locations-Header" {...getToggleProps({ 
-                onClick: () => setExpanded((prevExpanded) => !prevExpanded), 
-                })}>
-                {isExpanded ? 
-                <div className="container">
-                    <div className="row">
-                        <div className="col">
-                            Seleccione una opción
-                        </div>
-                        <div className="col-1">
-                        
-                        </div>
-                        <div className="col">
-                            <span className="Locations-Header-Arrow">&#10148;</span>
-                        </div>
-                    </div>
-                </div> : 
-                <div className="container">
-                    <div className="row">
-                        <div className="col">
-                            Seleccione una opción
-                        </div>
-                        <div className="col-1">
-                        
-                        </div>
-                        <div className="col">
-                            <span className="Locations-Header-Arrow">&#10148;</span>
-                        </div>
-                    </div>
-                </div>}
-            </div>
-            <div {...getCollapseProps()}>
-                <hr></hr>
-                <div className="Locations-Content">
-                    <div class="row">
-                        <p className="Locations-Content-Text">Muestra localidades</p>
-                    </div>               
-                </div>
-            </div>
-        </div>
-    )
-}
 
 export const Search = (props) => {
-    //const [ detailTrip, setDetailTrip ] = useContext(TripContext);
+    const [ detailTrip, setDetailTrip ] = useContext(TripContext);
 
     const [isExpanded, setExpanded] = useState(false)
     const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded });
@@ -91,48 +44,97 @@ export const Search = (props) => {
 
     function onClick(pId) {
         props.onClick();
-        //setDetailTrip(pId);
-        //console.log(detailTrip);
+        setDetailTrip(pId);
+        console.log(detailTrip);
     }
+
+    const [formValues, handleInputChange] = useForm({});
+
+    const { localidadO, localidadD } = formValues;
 
     return (
         <div className="Search">
-            <div className="Search-Header" {...getToggleProps({ 
-                onClick: () => setExpanded((prevExpanded) => !prevExpanded), 
-                })}>
-                {isExpanded ? "¿A donde vas?" : "¿A donde vas?"}
-            </div>
-            <div {...getCollapseProps()}>
-                <div className="container">
-                    <div className="Search-Content">
-                        <div className="row">
-                            <div className="col">
-                                <p className="Search-Content-Text">Localidad:</p>
-                            </div>
-                            <div className="col"></div>
-                            <div className="col"></div>
-                        </div>
-                        <div className="row">
-                            <div className="col"></div>
-                            <div className="col">
-                                <LocationsCollapsible></LocationsCollapsible>
-                            </div>
-                            <div className="col"></div>
-                        </div>
-                        <div className="row">
-                            {trips.map((elm, index) => (
-                                <Post onClick={() => {onClick(elm);}}
-                                    key={index}
-                                    _id={elm._id}
-                                    hour={elm.hour}
-                                    route={elm.route}
-                                    cost={elm.cost}
-                                ></Post>
-                            ))}
-                        </div>                      
+          <div
+            className="Search-Header"
+            {...getToggleProps({
+              onClick: () => setExpanded((prevExpanded) => !prevExpanded),
+            })}
+          >
+            {isExpanded ? "¿A donde vas?" : "¿A donde vas?"}
+          </div>
+          <div {...getCollapseProps()}>
+            <div className="container">
+              <div className="Search-Content">
+                <div className="row">
+                  <div className="col">
+                    <p className="Search-Content-Text">Localidad Origen:</p>
+                  </div>
+                  <div className="col">
+                    <div className="form-group">
+                      <select
+                        id="localidadOrigen"
+                        className="form-control"
+                        name="Localidad Origen"
+                        value={localidadO}
+                        onChange={handleInputChange}
+                      >
+                        <option className="form-option" selected>
+                          Seleccione una opción...
+                        </option>
+                        <option className="form-option">Uniandes</option>
+                        <option className="form-option">Barrios Unidos</option>
+                        <option className="form-option">Chapinero</option>
+                        <option className="form-option">Engativá</option>
+                        <option className="form-option">Fontibón</option>
+                        <option className="form-option">Puente Aranda</option>
+                      </select>
                     </div>
+                  </div>
+                  <div className="col"><p>{localidadO}</p></div>
                 </div>
+                <div className="row">
+                  <div className="col">
+                    <p className="Search-Content-Text">Localidad Destino:</p>
+                  </div>
+                  <div className="col">
+                    <div className="form-group">
+                      <select
+                        id="localidadDestino"
+                        className="form-control"
+                        name="Localidad Destino"
+                        value={localidadD}
+                        onChange={handleInputChange}
+                      >
+                        <option className="form-option" selected>
+                          Seleccione una opción...
+                        </option>
+                        <option className="form-option">Uniandes</option>
+                        <option className="form-option">Barrios Unidos</option>
+                        <option className="form-option">Chapinero</option>
+                        <option className="form-option">Engativá</option>
+                        <option className="form-option">Fontibón</option>
+                        <option className="form-option">Puente Aranda</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="col"></div>
+                </div>
+                <div className="row">
+                  {trips.map((elm, index) => (
+                    <Post
+                      onClick={() => {
+                        onClick(elm);
+                      }}
+                      key={index}
+                      hour={elm.hour}
+                      route={elm.route}
+                      cost={elm.cost}
+                    ></Post>
+                  ))}
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    )
+      );
 }
