fca8bac2714ab5733d0d168b41c286563c21fa48
David Ruiz <daleruvi@gmail.com>
2022-06-01 14:11:39 -0500

    Footer example

---
+++ front/src/components/Footer/Footer.js
@@ -0,0 +1,52 @@
+export const Footer = () => {
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"
+      />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css"
+      />
+      <div className="footer-basic">
+        <footer>
+          <div className="social">
+            <a href="#">
+              <i className="icon ion-social-instagram"></i>
+            </a>
+            <a href="#">
+              <i className="icon ion-social-snapchat"></i>
+            </a>
+            <a href="#">
+              <i className="icon ion-social-twitter"></i>
+            </a>
+            <a href="#">
+              <i className="icon ion-social-facebook"></i>
+            </a>
+          </div>
+          <ul className="list-inline">
+            <li className="list-inline-item">
+              <a href="#">Home</a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#">Services</a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#">About</a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#">Terms</a>
+            </li>
+            <li className="list-inline-item">
+              <a href="#">Privacy Policy</a>
+            </li>
+          </ul>
+          <p className="copyright">Wheels App © 2022</p>
+        </footer>
+      </div>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+    </>
+  );
+};
--- front/src/pages/home/Home.js
+++ front/src/pages/home/Home.js
@@ -1,120 +1,163 @@
-
 import React, { useState, useContext, useEffect } from "react";
-import { UserContext } from './../../context/UserContext';
-import { Navigate } from 'react-router-dom';
+import { UserContext } from "./../../context/UserContext";
+import { Navigate } from "react-router-dom";
 
-import './Home.scss';
+import "./Home.scss";
 
-import { Map } from '../../components/Map/Map';
-import { Search } from './../../components/Search/Search';
+import { Map } from "../../components/Map/Map";
+import { Search } from "./../../components/Search/Search";
 import { Detail } from "../../components/Detail/Detail";
 import { Sidebar } from "../../components/Sidebar/Sidebar";
 import { MenuBtn } from "../../components/MenuBtn/MenuBtn";
-import driverNoActive from './../../data/driverNoActive.png';
-import passengerActive from './../../data/passengerActive.png';
-import driverActive from './../../data/driverActive.png';
-import passengerNoActive from './../../data/passengerNoActive.png';
+import driverNoActive from "./../../data/driverNoActive.png";
+import passengerActive from "./../../data/passengerActive.png";
+import driverActive from "./../../data/driverActive.png";
+import passengerNoActive from "./../../data/passengerNoActive.png";
+import { Footer } from "../../components/Footer/Footer";
 
 import { CrearViaje } from "../crearViaje/CrearViaje";
 
-import Popup from 'reactjs-popup';
-import 'reactjs-popup/dist/index.css';
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 import { TripContext } from "../../context/TripContext";
 
 export const Home = () => {
-    const [showResults, setShowResults] = useState(false)
-    var id = ''
+  const [showResults, setShowResults] = useState(false);
+  var id = "";
 
-    const onClick = (event) => {
-        setShowResults(true);
+  const onClick = (event) => {
+    setShowResults(true);
+  };
+
+  const { setUser, user } = useContext(UserContext);
+
+  const [pMode, changeMode] = useState(true);
+  console.log(pMode);
+
+  const onClick2 = () => setShowResults(false);
+
+  useEffect(() => {
+    const userLocalStorage = localStorage.getItem("username");
+
+    if (userLocalStorage != null) {
+      setUser({
+        name: userLocalStorage,
+        token: localStorage.getItem("token"),
+      });
     }
+  }, [setUser]);
 
-    const { setUser, user } = useContext(UserContext);
-
-    const [pMode, changeMode] = useState(true)
-    console.log(pMode)
-    
-
-    const onClick2 = () => setShowResults(false)
-
-    useEffect(() => {
-        const userLocalStorage = localStorage.getItem('username');
-
-        if(userLocalStorage != null){
-            setUser({
-                name: userLocalStorage,
-                token: localStorage.getItem('token')
-            });
-        }
-    }, [setUser]);
-
-    const [detailTrip, setDetailTrip] = useState("");
-    /*function callbackTripDetail(tripId){
+  const [detailTrip, setDetailTrip] = useState("");
+  /*function callbackTripDetail(tripId){
         id = tripId
     };*/
 
-    /*function openNav() {
+  /*function openNav() {
         document.getElementsByClassName("2").style.width = "250px";
         document.getElementsByTagName("Home").style.marginLeft = "0px";
     }
       
       /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-    /*function closeNav() {
+  /*function closeNav() {
         document.getElementsByClassName("2").style.width = "0";
         document.getElementsByTagName("Home").style.marginLeft = "0";
     }*/
 
-    return(
-        <>
-            <div className="Home">
-                {user && (<>
-                    <div className="Back" id="Back"></div>
-                    {pMode?
-                    <div className="Menu">
-                        <TripContext.Provider value={[detailTrip, setDetailTrip]}>
-                            <button className='Menu-Search'>{ showResults ? <Detail onClick={onClick2}/> : <Search onClick={onClick}/> }</button>
-                        </TripContext.Provider>
-                        <div className="Menu-Buttons">
-                            <div className='Menu-Buttons-Style'>
-                                <img alt='PasajeroIcon' src={passengerActive} className='Menu-Buttons-Style-Img' />
-                                <p className='Menu-Buttons-Style-Text'>Pasajero</p>
-                            </div>
-                            <div className='Menu-Buttons-Style'>
-                                <img alt='ConductorIcon' src={driverNoActive} className='Menu-Buttons-Style-Img' />
-                                <p onClick={() =>changeMode(false)} className='Menu-Buttons-Style-Text'>Conductor</p>
-                            </div>
-                        </div>
-                    </div>
-                 :
-                    <div className="Menu">
-                        <Popup trigger={<button className='Menu-Search'> Crear Viaje</button>} position="top center">
-                            <CrearViaje />
-                        </Popup>
-                        <div className="Menu-Buttons">
-                            <div className='Menu-Buttons-Style'>
-                                <img alt='PasajeroIcon' src={passengerNoActive} className='Menu-Buttons-Style-Img' />
-                                <p onClick={() =>changeMode(true)} className='Menu-Buttons-Style-Text'>Pasajero</p>
-                            </div>
-                            <div className='Menu-Buttons-Style'>
-                                <img alt='ConductorIcon' src={driverActive} className='Menu-Buttons-Style-Img' />
-                                <p className='Menu-Buttons-Style-Text'>Conductor</p>
-                            </div>
-                        </div>
-                    </div>
-                }
-                <Map></Map>
-                <div id='logout-container'>
-                        <button className='btn btn-warning' onClick={() => {
-                                                                    localStorage.removeItem('username');
-                                                                    localStorage.removeItem('token');
-                                                                    setUser(null);}}>
-                            Logout
-                        </button>
-                    </div></>
-                )}
-                {!user && (<Navigate to="/" />)}
+  return (
+    <>
+      <div className="Home">
+        {user && (
+          <>
+            <div className="Back" id="Back"></div>
+            {pMode ? (
+              <div className="Menu">
+                <TripContext.Provider value={[detailTrip, setDetailTrip]}>
+                  <button className="Menu-Search">
+                    {showResults ? (
+                      <Detail onClick={onClick2} />
+                    ) : (
+                      <Search onClick={onClick} />
+                    )}
+                  </button>
+                </TripContext.Provider>
+                <div className="Menu-Buttons">
+                  <div className="Menu-Buttons-Style">
+                    <img
+                      alt="PasajeroIcon"
+                      src={passengerActive}
+                      className="Menu-Buttons-Style-Img"
+                    />
+                    <p className="Menu-Buttons-Style-Text">Pasajero</p>
+                  </div>
+                  <div className="Menu-Buttons-Style">
+                    <img
+                      alt="ConductorIcon"
+                      src={driverNoActive}
+                      className="Menu-Buttons-Style-Img"
+                    />
+                    <p
+                      onClick={() => changeMode(false)}
+                      className="Menu-Buttons-Style-Text"
+                    >
+                      Conductor
+                    </p>
+                  </div>
+                </div>
+                <Footer></Footer>
+              </div>
+            ) : (
+              <div className="Menu">
+                <Popup
+                  trigger={
+                    <button className="Menu-Search"> Crear Viaje</button>
+                  }
+                  position="top center"
+                >
+                  <CrearViaje />
+                </Popup>
+                <div className="Menu-Buttons">
+                  <div className="Menu-Buttons-Style">
+                    <img
+                      alt="PasajeroIcon"
+                      src={passengerNoActive}
+                      className="Menu-Buttons-Style-Img"
+                    />
+                    <p
+                      onClick={() => changeMode(true)}
+                      className="Menu-Buttons-Style-Text"
+                    >
+                      Pasajero
+                    </p>
+                  </div>
+                  <div className="Menu-Buttons-Style">
+                    <img
+                      alt="ConductorIcon"
+                      src={driverActive}
+                      className="Menu-Buttons-Style-Img"
+                    />
+                    <p className="Menu-Buttons-Style-Text">Conductor</p>
+                  </div>
+                </div>
+                <Footer></Footer>
+              </div>
+            )}
+            <Map></Map>
+            <div id="logout-container">
+              <button
+                className="btn btn-warning"
+                onClick={() => {
+                  localStorage.removeItem("username");
+                  localStorage.removeItem("token");
+                  setUser(null);
+                }}
+              >
+                Logout
+              </button>
             </div>
-        </>
-    );
-    
+          </>
+        )}
+        {!user && <Navigate to="/" />}
+      </div>
+    </>
+  );
 };
